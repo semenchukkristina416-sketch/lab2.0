5c5953598d1e3cbabfcc519f34c7d27dbc7ad59a
semenchukkristina416-sketch <semenchukkristina416@gmail.com>
2025-09-25 23:23:33 +0300

    Оновлено код

--- laboratorna2.0.cpp
+++ laboratorna2.0.cpp
@@ -1,27 +1,33 @@
 // Lab_02.cpp
 // <Семенчук Крістіна>
-//  Лабораторна робота № 2
+// Лабораторна робота № 2
 // Лінійні програми.
 // Варіант 22
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 int main()
 {
-	double alpha; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	double z2;// результат обчислення 2-го
-	cout << "alpha="; cin >> alpha;
-	cout << "cos(alpha)=" << cos(alpha) << endl;
-	cout << "sin(alpha) = " << sin(alpha) << endl;
-	z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
-	z2 = 2 * sqrt(2) * cos(alpha) * sin(M_PI / 4 + 2 * alpha);
+    double alpha; // вхідний параметр
+    double z1;    // результат обчислення 1-го виразу
+    double z2;    // результат обчислення 2-го
 
+    cout << "alpha=";
+    cin >> alpha;
 
-	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
-	cin.get();
-	return 0;
+    cout << "cos(alpha)=" << cos(alpha) << endl;
+    cout << "sin(alpha)=" << sin(alpha) << endl;
+
+    z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
+    z2 = 2 * sqrt(2) * cos(alpha) * sin(M_PI / 4 + 2 * alpha);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    cin.get(); // для очікування Enter перед закриттям
+    return 0;
 }
